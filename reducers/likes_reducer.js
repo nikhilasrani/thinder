85095e9daaeed53043c8f2f2c26259ff523559a7
Nikhil Asrani <asrani.nikhil@gmail.com>
2019-05-06 01:46:32 +0530

    added redux-persist for persisting store

--- reducers/likes_reducer.js
+++ reducers/likes_reducer.js
@@ -1,8 +1,11 @@
 import _ from "lodash";
+import { REHYDRATE } from "redux-persist/lib/constants";
 import { LIKE_JOB, CLEAR_LIKED_JOBS } from "../actions/types";
 
 export default function(state = [], action) {
   switch (action.type) {
+    case REHYDRATE:
+      return action.payload.likedJobs || [];
     case LIKE_JOB:
       return _.uniqBy([action.payload, ...state], "restaurant.id");
     case CLEAR_LIKED_JOBS:
